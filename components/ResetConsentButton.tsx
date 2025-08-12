2ee60e818816a96a1ad7a7a8fa43ca5c7828551e
Oajdea Alin <32819761+alinoajdea@users.noreply.github.com>
2025-08-12 09:42:39 +0300

    Update ResetConsentButton.tsx

--- components/ResetConsentButton.tsx
+++ components/ResetConsentButton.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+const CONSENT_EVENT = "tf-consent-changed";
+
 export function ResetConsentButton() {
   return (
     <button
       className="text-sm text-blue-600 underline"
       onClick={() => {
-        document.cookie = "tf_consent=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-        window.dispatchEvent(new Event("tf-consent"));
-        // reload to show banner again
+        document.cookie = "tf_consent=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT; SameSite=Lax";
+        window.dispatchEvent(new Event(CONSENT_EVENT));
         window.location.reload();
       }}
     >
